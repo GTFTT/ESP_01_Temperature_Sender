0b9a154576ec89b1b6666e36f6de25f2d33bd455
GT <FTowerT@outlook.com>
2021-11-05 00:26:22 +0200

    Added working sketch for sending datat to the server

--- ESP_01_Temperature_Sender.ino
+++ ESP_01_Temperature_Sender.ino
@@ -1,9 +1,54 @@
-// the loop function runs over and over again forever
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "Nataliya";
+const char* password = "08061961";
+
+//Your Domain name with URL path or IP address with path
+String serverName = "http://8eb9-92-112-198-38.ngrok.io/updateDeviceData/44rDuq17";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(2, HIGH); //Indicate that all is ready
+}
+
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because 
-                                    // it is acive low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  // Send an HTTP POST request depending on timerDelay
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+
+      http.begin(client, serverName.c_str());
+      
+      // Send HTTP GET request
+      int httpCode = http.PATCH("{\"data\": {\"hi\": 123}}");   //Send the request
+      String payload = http.getString();                  //Get the response payload
+  
+      Serial.println(httpCode);   //Print HTTP return code
+      Serial.println(payload);    //Print request response payload
+  
+      http.end();  //Close connection
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
