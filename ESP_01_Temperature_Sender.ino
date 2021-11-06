9ddf9865f8a2deb0c9454d184f92a9846e55bd1d
GT <FTowerT@outlook.com>
2021-11-06 14:48:50 +0200

    New working sketch that is able to send request to the server

--- ESP_01_Temperature_Sender.ino
+++ ESP_01_Temperature_Sender.ino
@@ -1,45 +1,40 @@
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <ArduinoJson.h>
+#include <Chrono.h>
+
+#define sendInterval 1000
 
 const char* ssid = "Nataliya";
 const char* password = "08061961";
 
-//Your Domain name with URL path or IP address with path
-String serverName = "http://8eb9-92-112-198-38.ngrok.io/updateDeviceData/44rDuq17";
+Chrono      senderChrono;
 
-unsigned long lastTime = 0;
-unsigned long timerDelay = 1000;
+String requestPath = "http://a2b7-92-112-198-38.ngrok.io/updateDeviceData/44rDuq17";
 
 void setup() {
   Serial.begin(115200);
+  connectToWiFi();
   pinMode(2, OUTPUT);
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
   digitalWrite(2, HIGH); //Indicate that all is ready
 }
 
 void loop() {
-  // Send an HTTP POST request depending on timerDelay
-  if ((millis() - lastTime) > timerDelay) {
+  if(senderChrono.hasPassed(sendInterval, true)) {
     //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
+    if(WiFi.status() == WL_CONNECTED){
       WiFiClient client;
       HTTPClient http;
 
-      http.begin(client, serverName.c_str());
+      http.begin(client, requestPath.c_str());
+
+      String message = getDataMessage();
+//      String message = "{\"data\": {\"val\": 12}}";
+      Serial.println((String)"Message: " + message);
       
       // Send HTTP GET request
-      int httpCode = http.PATCH("{\"data\": {\"hi\": 123}}");   //Send the request
+      int httpCode = http.PATCH(message.c_str());   //Send the request
       String payload = http.getString();                  //Get the response payload
   
       Serial.println(httpCode);   //Print HTTP return code
@@ -47,8 +42,41 @@ void loop() {
   
       http.end();  //Close connection
     } else {
-      Serial.println("WiFi Disconnected");
+      Serial.println(F("WiFi connection lost/not connected. Atrtempt to reconnect."));
+      connectToWiFi();
     }
-    lastTime = millis();
+  }
+}
+
+//Generate json ready to be sent
+String getDataMessage() {
+  DynamicJsonDocument doc(512);
+  doc["temperature"] = 10.00;
+  doc["humidity"] = 10.00;
+  
+  String message;
+  serializeJson(doc, message);
+
+  return message;
+}
+
+void connectToWiFi() {
+  // Connect to wifi
+  WiFi.begin(ssid, password);
+
+  // Wait some time to connect to wifi
+  Serial.print("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  // Check if connected to wifi
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.print(F("Connected to WiFi network with IP Address: "));
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("Failded to connect to Wifi!");
   }
 }
